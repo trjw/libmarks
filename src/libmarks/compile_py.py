bff7f63403d78646847a64a1731615e0fb156a95
Joel Addison <joel@addison.net.au>
2014-05-01 06:28:26 +0000

    Marks: Add python compilation
    Add Makefile to compile python code and put the .pyc files
    into the main package folder.
    
    
    git-svn-id: https://svn.geocomp.uq.edu.au/svn/jeathesis@12 a4bfb809-27fc-4812-b90d-531e79a1b4ec

---
+++ src/libmarks/compile_py.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import fnmatch
+import py_compile
+
+
+def compile(source_dir, dest_dir):
+    for root, dirs, files in os.walk(source_dir):
+        # Get path to file, relative to source_dir
+        curr_dir = os.path.relpath(root, source_dir)
+        if curr_dir == '.':
+            curr_dir = ''
+
+        # Compile all py files and put them in dest_dir
+        py_files = fnmatch.filter(files, '*.py')
+        for f in py_files:
+            py_compile.compile(
+                os.path.join(root, f),
+                os.path.join(dest_dir, curr_dir, f + 'c'))
+
+        # Create all dirs within dest_dir
+        for d in dirs:
+            os.mkdir(os.path.join(dest_dir, d))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 3:
+        sys.exit("Usage: compile_py.py source_dir dest_dir")
+
+    source_dir = sys.argv[1]
+    dest_dir = sys.argv[2]
+    compile(source_dir, dest_dir)
