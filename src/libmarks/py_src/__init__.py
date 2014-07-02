8899ea0da1acea353181eeef5d31702cf25c6050
Joel Addison <joel@addison.net.au>
2014-07-02 03:26:54 +0000

    Marks: Make available components explicit
    git-svn-id: https://svn.geocomp.uq.edu.au/svn/jeathesis@29 a4bfb809-27fc-4812-b90d-531e79a1b4ec

--- src/libmarks/py_src/__init__.py
+++ src/libmarks/py_src/__init__.py
@@ -4,7 +4,12 @@ MARKS - systems programming testing and marking framework.
 Based on Python unittest framework.
 """
 
-from .process import *
+__all__ = [
+    'main', 'Process', 'TestCase', 'TestSuite', 'TestLoader',
+    'default_test_loader', 'TestResult'
+]
+
+from .process import Process
 from .case import TestCase
 from .suite import TestSuite
 from .loader import TestLoader, default_test_loader
