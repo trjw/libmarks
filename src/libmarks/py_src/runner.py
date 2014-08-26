a378bdb69471bab542cb98f0a1daf23f483e841f
Joel Addison <joel@addison.net.au>
2014-08-26 13:18:23 +0000

    Marks: Add temporary folder environment setup
    Create a temporary folder to run all tests inside. Folder will be removed
    upon tests finishing if cleanup is enabled.
    
    git-svn-id: https://svn.geocomp.uq.edu.au/svn/jeathesis@70 a4bfb809-27fc-4812-b90d-531e79a1b4ec

--- src/libmarks/py_src/runner.py
+++ src/libmarks/py_src/runner.py
@@ -1,5 +1,11 @@
 from __future__ import division, print_function
+import os
+import errno
+import tempfile
+import shutil
 from . import result
+
+TEMP_PREFIX = 'testres'
 
 
 class BasicTestRunner(object):
@@ -12,18 +18,65 @@ class BasicTestRunner(object):
         if result_class is not None:
             self.result_class = result_class
 
+        # Store the working directory.
+        self.flags['working_dir'] = os.getcwd()
+
     def _apply_flags(self, test):
         """Apply the appropriate flags to the tests"""
         test.__marks_flags__ = self.flags
 
-    def run(self, test):
-        result = self.result_class()
+    def setup_environment(self):
+        """Setup the environment before running the tests.
 
+        Create a temporary directory to run tests from. Store location
+        in flags, so test cases can use this information.
+        """
+        working_dir = self.flags['working_dir']
+        prefix = self.flags.get('temp_prefix', TEMP_PREFIX)
+
+        # Create temporary working directory.
+        temp_dir = tempfile.mkdtemp(dir=working_dir, prefix=prefix)
+
+        if not self.flags.get('silent', False):
+            print("Test output in", temp_dir)
+            print("Remember to clean up your test result folders.\n")
+
+        # Change to new temp directory.
+        os.chdir(temp_dir)
+
+        # Store temporary directory path.
+        self.flags['temp_dir'] = temp_dir
+
+    def tear_down_environment(self):
+        """Tear down the environment after running the tests"""
+        if self.flags.get('cleanup', False):
+            try:
+                shutil.rmtree(self.flags['temp_dir'])
+            except OSError as e:
+                # Check for ENOENT: No such file or directory.
+                # (Nothing more to do if directory has already been deleted)
+                if e.errno != errno.ENOENT:
+                    # Other error, so raise.
+                    raise
+
+    def run(self, test):
+        # Setup the environment.
+        self.setup_environment()
+
+        # Apply the flags to the test.
         self._apply_flags(test)
 
+        # Create the result holder.
+        result = self.result_class()
+
+        # Run the test
         result.start_test_run()
         test.run(result)
         result.stop_test_run()
+
+        # Tear down the environment.
+        self.tear_down_environment()
+
         return result
 
 
@@ -32,6 +85,13 @@ class MarkingTestRunner(BasicTestRunner):
     result_class = result.MarkingTestResult
 
     def run(self, test):
+        # Setup the environment.
+        self.setup_environment()
+
+        # Apply the flags to the test.
+        self._apply_flags(test)
+
+        # Create the result holder.
         result = self.result_class()
 
         result.start_test_run()
@@ -39,21 +99,25 @@ class MarkingTestRunner(BasicTestRunner):
         result.stop_test_run()
 
         # Print results
-        print("{0:30}{1:15}{2}".format('Category', 'Passed', 'Mark'))
-        for category in result.marks:
-            info = result.marks[category]
-            total = info['total_marks'] or info['category_marks']
-            print("{0:30}{1:15}{2:.2f}/{3:.2f} ({4:.2%})".format(
-                category,
-                '{0}/{1}'.format(info['passed'], len(info['tests'])),
-                info['mark'], total,
-                info['mark'] / total))
+        if not self.flags.get('silent', False):
+            print("{0:30}{1:15}{2}".format('Category', 'Passed', 'Mark'))
+            for category in result.marks:
+                info = result.marks[category]
+                total = info['total_marks'] or info['category_marks']
+                print("{0:30}{1:15}{2:.2f}/{3:.2f} ({4:.2%})".format(
+                    category,
+                    '{0}/{1}'.format(info['passed'], len(info['tests'])),
+                    info['mark'], total,
+                    info['mark'] / total))
 
-        print("\n{0:30}{1:15}{2:.2f}/{3:.2f} ({4:.2%})".format(
-            'Total',
-            '{0}/{1}'.format(result.tests_passed, result.total_tests),
-            result.received_marks, result.total_marks,
-            result.received_marks / result.total_marks))
+            print("\n{0:30}{1:15}{2:.2f}/{3:.2f} ({4:.2%})".format(
+                'Total',
+                '{0}/{1}'.format(result.tests_passed, result.total_tests),
+                result.received_marks, result.total_marks,
+                result.received_marks / result.total_marks))
+
+        # Tear down the environment
+        self.tear_down_environment()
 
         return result
 
@@ -71,7 +135,7 @@ class UpdateTestRunner(BasicTestRunner):
     result_class = result.UpdateTestResult
 
     def run(self, test):
-        # Confirm that the update should proceed
+        # Confirm that the update should proceed.
         print(CONFIRMATION_MESSAGE)
         confirm = raw_input("Are you sure you want to update the files? (y/N)")
 
