7eb8a1dd324b763a832fd618a9fc4371c77dbf2c
Joel Addison <joel@addison.net.au>
2014-09-18 13:07:12 +0000

    Marks: Move to version 0.4a1
    git-svn-id: https://svn.geocomp.uq.edu.au/svn/jeathesis@94 a4bfb809-27fc-4812-b90d-531e79a1b4ec

--- src/libmarks/py_src/_version.py
+++ src/libmarks/py_src/_version.py
@@ -4,7 +4,7 @@ Used from Django:
     https://github.com/django/django/blob/master/django/utils/version.py
 """
 
-VERSION = (0, 3, 0, 'alpha', 4)
+VERSION = (0, 4, 0, 'alpha', 1)
 
 
 def get_version(version=None):
