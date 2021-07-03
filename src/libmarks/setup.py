ed89ad94f404f3fb34f2315d5c03fa714bca42f9
John Williams <j.williams1@uq.edu.au>
2021-07-03 23:10:34 +1000

    Support building libmarks using the py setup tools instead of custom script

---
+++ src/libmarks/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+import subprocess
+
+from distutils.command.install import install as DistutilsInstall
+
+class BoostLibInstall(DistutilsInstall):
+    def run(self):
+        makeProc=subprocess.Popen('make');
+        rc=makeProc.wait()
+        #raise OSError()
+        if rc != 0:
+            raise OSError()
+        DistutilsInstall.run(self)
+
+        print("post", file=sys.stderr);
+
+boostMacros = [('BOOST_BIND_GLOBAL_PLACEHOLDERS', '1')]
+boostLibs = ['boost_python38']
+
+moduleProcess = Extension('marks.process', sources=['src/process.cpp', 'src/glue.cpp', 
+                                             'src/traced_process.cpp'],
+                            libraries = boostLibs,
+                            define_macros = boostMacros)
+moduleProtect = Extension('libprotect', sources=['src/protection.c'])
+
+setup(name='marks', version='1.0',
+        description='CSSE2310 libmarks',
+        author='various',
+        packages=['marks'],
+        ext_modules = [moduleProcess, moduleProtect],
+        #cmdclass={'install': BoostLibInstall},
+        )
